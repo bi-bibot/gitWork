bf92a4ff54c8307d76a8b2e7d52edaaf9b943bf7
Babiscuzz_ <fate8086@gmail.com>
2024-11-26 15:47:35 +0100

    create webpage

---
+++ index.js
@@ -0,0 +1,7 @@
+// Function to display a greeting message
+function greetUser() {
+    alert("Hello, welcome to my simple website!");
+}
+
+// Add event listener to the button
+document.getElementById('clickMeButton').addEventListener('click', greetUser);
