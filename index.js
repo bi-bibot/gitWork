ae743bbb32df031e109d100e306e51df27a38e4f
Babiscuzz_ <fate8086@gmail.com>
2024-11-27 14:49:23 +0100

    repull the working code

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ const http = require('http');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 
-
-
-http.listen(port);
-http.get("/listBooks", (request, response) => {
-    const status = {
-        "Status" : "Runnung"
-    };
-
-    response.write(status);
-})
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('index.html', function(error, data)
+    {
+        if(error)
+        {
+            response.writeHead(404);
+            response.write('Error : File Not Found')
+        }
+        else
+        {
+            response.write(data);
+        }
+ 
+        response.end()
+    })
+   
+ 
+ 
+   
+ 
+}).listen(port);
 
 console.log(`Server running at http://localhost:${port}`); 
