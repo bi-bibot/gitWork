9338830dc3735acfff8f522cade9402c4470b3c2
Babiscuzz_ <fate8086@gmail.com>
2024-11-27 13:47:27 +0100

    update getListBooks

--- getListBooks.js
+++ getListBooks.js
@@ -2,5 +2,13 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 const path = '/listBooks';
 
-http.get()
+http.listen(port);
+
+http.get("/listBooks", (request, response) => {
+    const status = {
+        "Status" : "Runnung"
+    };
+
+    response.write(status);
+})
 
