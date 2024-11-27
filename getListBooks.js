1b512a58f83ee3c638d9d74a900ab7d958539c49
Babiscuzz_ <fate8086@gmail.com>
2024-11-27 13:10:54 +0100

    deploy get URL function

---
+++ getListBooks.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+const path = '/listBooks';
+
+http.get()
+
--- index.js
+++ index.js
@@ -4,22 +4,47 @@ const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('index.html', function(error, data)
-    {
-        if(error)
-        {
-            response.writeHead(404);
-            response.write('Error : File Not Found')
-        }
-        else
-        {
-            response.write(data);
-        }
 
-        response.end() 
-    })
+    if(request.url == '/listBooks')
+    {
+        fs.readFile('listBooks.html', function(error, data)
+        {
+            if(error)
+            {
+                response.writeHead(404);
+                response.write('Error : File Not Found')
+            }
+            else
+            {
+                response.write(data);
+            }
+
+            response.end() 
+        })
+
+    }
+    else
+    {
+        fs.readFile('index.html', function(error, data)
+        {
+            if(error)
+            {
+                response.writeHead(404);
+                response.write('Error : File Not Found')
+            }
+            else
+            {
+                response.write(data);
+            }
+
+            response.end() 
+        })
+    }
+
+    
   
 }).listen(port);
 
+http.createServer()
 
 console.log(`Server running at http://localhost:${port}`); 
