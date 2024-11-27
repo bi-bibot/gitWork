8a123e17697efac11110788e8d1b8aec63922c45
Babiscuzz_ <fate8086@gmail.com>
2024-11-27 12:40:13 +0100

    deletd get function from index.js

--- index.js
+++ index.js
@@ -21,24 +21,5 @@ http.createServer(function(request, response) {
   
 }).listen(port);
 
-http.get('/listBooks', function(request,response)
-{
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('listBooks.html', function(error, data)
-    {
-        if(error)
-        {
-            response.writeHead(404);
-            response.write('Error : File Not Found')
-        }
-        else
-        {
-            response.write(data);
-        }
-
-        response.end() 
-    })
-})
-
 
 console.log(`Server running at http://localhost:${port}`); 
