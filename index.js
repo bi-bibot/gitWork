7f5a79bf15be30d591511a5deee92ec90e478278
Babiscuzz_ <fate8086@gmail.com>
2024-11-27 14:43:43 +0100

    deleted createServer method

--- index.js
+++ index.js
@@ -2,29 +2,9 @@ const http = require('http');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 
-http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('index.html', function(error, data)
-    {
-        if(error)
-        {
-            response.writeHead(404);
-            response.write('Error : File Not Found')
-        }
-        else
-        {
-            response.write(data);
-        }
-
-        response.end() 
-    })
-    
 
 
-    
-  
-}).listen(port);
-
+http.listen(port);
 http.get("/listBooks", (request, response) => {
     const status = {
         "Status" : "Runnung"
