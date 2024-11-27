04b8172628b60e2bb17fc37c3cbfcde3d330ad67
Babiscuzz_ <fate8086@gmail.com>
2024-11-27 10:00:34 +0100

    yes

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World!');
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('index.html', function(error, data)
+    {
+        if(error)
+        {
+            response.writeHead(404);
+            response.write('Error : File Not Found')
+        }
+        else
+        {
+            response.write(data);
+        }
+
+        response.end()
+    })
+  
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
