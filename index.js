139ec0a83c8c60e85789060bccc97c51effcef3b
Babiscuzz_ <fate8086@gmail.com>
2024-11-28 11:45:03 +0100

    port to 8080

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const fs = require('fs');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html' });
