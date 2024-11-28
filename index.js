28468c8cc78b25438f3456a096c3b470161ff97f
Babiscuzz_ <fate8086@gmail.com>
2024-11-28 09:08:08 +0100

    try

--- index.js
+++ index.js
@@ -3,38 +3,12 @@ const fs = require('fs');
 const port = process.env.PORT || 3000;
 
 http.get('https://brofuckingwork.azurewebsites.net', (res) => {
-    const { statusCode } = res;
-    const contentType = res.headers['content-type'];
+    const { statusCode } = response;
+    const contentType = response.headers['content-type'];
   
-    let error;
-    // Any 2xx status code signals a successful response but
-    // here we're only checking for 200.
-    if (statusCode !== 200) {
-      error = new Error('Request Failed.\n' +
-                        `Status Code: ${statusCode}`);
-    } else if (!/^application\/json/.test(contentType)) {
-      error = new Error('Invalid content-type.\n' +
-                        `Expected application/json but received ${contentType}`);
-    }
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('Hello');
 
-    if (error) {
-      console.error(error.message);
-      // Consume response data to free up memory
-      res.resume();
-      return;
-    }
-  
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        console.log(parsedData);
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
   }).on('error', (e) => {
     console.error(`Got error: ${e.message}`);
   });
